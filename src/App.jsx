7c50f2676d8e00817b049c54c7ba4b934e710417
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-11-18 06:57:34 -0800

    contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, BrowserRouter as Router, Routes} from "react-router-dom"
 import Navbar from "./components/Navbar"
-import { Home, About, Projects } from "./pages"
+import { Home, About, Projects, Contact } from "./pages"
 
 
 const App = () => {
@@ -12,6 +12,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
       </Router>
     </main>
